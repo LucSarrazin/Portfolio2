8034defb7418938a626b5c2a481a50906a946e61
LucSarrazin <157165004+LucSarrazin@users.noreply.github.com>
2024-07-30 19:08:24 +0200

    Update contact.php

--- contact.php
+++ contact.php
@@ -7,7 +7,7 @@
 
 <body>
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "GET") {
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name = htmlspecialchars($_POST['name'] ?? '');
     $email = htmlspecialchars($_POST['email'] ?? '');
     $message = htmlspecialchars($_POST['message'] ?? '');
